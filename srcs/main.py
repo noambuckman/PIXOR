ba2f3606b9a1a92fd2c1606d98483408605e4de3
Noam Buckman <nbuckman@mit.edu>
2022-03-03 16:46:53 -0500

    detach earlier

--- srcs/main.py
+++ srcs/main.py
@@ -69,7 +69,7 @@ def eval_batch(config, net, loss_fn, loader, device, eval_range='all'):
             
             toc = time.time()
             # Parallel post-processing
-            predictions = list(torch.split(predictions.cpu(), 1, dim=0))
+            predictions = list(torch.split(predictions.cpu().detach(), 1, dim=0))
             batch_size = len(predictions)
             with Pool (processes=3) as pool:
                 preds_filtered = pool.starmap(filter_pred, [(config, pred) for pred in predictions])
@@ -84,7 +84,7 @@ def eval_batch(config, net, loss_fn, loader, device, eval_range='all'):
                 if image_id[j] in log_img_list:
                     # pass
                     input_np = input[j].cpu().detach().numpy().transpose(1, 2, 0)
-                    corners = corners.cpu().detach().numpy()
+                    # corners = corners.cpu().detach().numpy()
                     pred_image = get_bev(input_np, corners, None, geometry=config["geometry"])
                     log_images.append(pred_image)
 
