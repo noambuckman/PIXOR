b880fae315c7551668ea3943622cc715655c61ad
Noam Buckman <nbuckman@mit.edu>
2022-03-03 17:20:19 -0500

    keep logs

--- srcs/main.py
+++ srcs/main.py
@@ -86,7 +86,7 @@ def eval_batch(config, net, loss_fn, loader, device, eval_range='all'):
                     input_np = input[j].detach().cpu().numpy().transpose(1, 2, 0)
                     # # corners = corners.cpu().detach().numpy()
                     pred_image = get_bev(input_np, corners, None, geometry=config["geometry"])
-                    # log_images.append(pred_image)
+                    log_images.append(pred_image)
 
                 arg = (np.array(label_list), corners, scores)
                 args.append(arg)
