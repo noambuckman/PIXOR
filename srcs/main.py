ffdc26ac48f002ac812ac69cea6c5baaaf56ae57
Noam Buckman <nbuckman@mit.edu>
2022-03-03 18:07:05 -0500

    remove print

--- srcs/main.py
+++ srcs/main.py
@@ -82,11 +82,9 @@ def eval_batch(config, net, loss_fn, loader, device, eval_range='all'):
                 preds += len(scores)
                 all_scores.extend(list(scores))
                 if image_id[j] in log_img_list:
-                    # pass
                     input_np = input[j].detach().cpu().numpy()
                     input_np = input_np.transpose(1, 2, 0)
-                    # # corners = corners.cpu().detach().numpy()
-                    pred_image = get_bev(input_np, corners, None, geometry=config["geometry"])
+                    # pred_image = get_bev(input_np, corners, None, geometry=config["geometry"])
                     # log_images.append(pred_image)
 
                 arg = (np.array(label_list), corners, scores)
