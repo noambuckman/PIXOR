b827fe3d355f28306aba4317f19192a431d22634
Noam Buckman <nbuckman@mit.edu>
2022-03-09 19:02:32 -0500

    tolist fix

--- srcs/main.py
+++ srcs/main.py
@@ -195,9 +195,9 @@ def train(exp_name, device, clip=True, debug=False):
     target_mean = train_data_loader.dataset.target_mean
     target_std_dev = train_data_loader.dataset.target_std_dev
     print(target_mean.shape)
-    print(target_mean.to_list())
-    config["target_mean"] = target_mean.tolist()[0]
-    config["target_std_dev"] = target_std_dev.tolist()[0]
+    print(target_mean.tolist())
+    config["target_mean"] = target_mean.tolist()
+    config["target_std_dev"] = target_std_dev.tolist()
     save_config(config, exp_name)
 
     net, loss_fn, optimizer, scheduler = build_model(config, device, train=True, target_mean=target_mean, target_std_dev=target_std_dev)
