defd82da673fd34895ad89078a7e600e75e9abe5
Noam Buckman <nbuckman@mit.edu>
2022-03-10 15:03:14 -0500

    print finite

--- srcs/datagen.py
+++ srcs/datagen.py
@@ -344,6 +344,9 @@ class KITTI(Dataset):
             velo_processed[x, y, -1] += float(velo[i, 3])
             intensity_map_count[x, y] += 1.0
         
+        print(np.isfinite(velo_processed[:,:,-1]).all())
+        print(np.intensity_map_count(velo_processed[:,:,-1]).all())
+        print(" ")
         avg_intensity = np.where(intensity_map_count > 0.01, velo_processed[:, :, -1] / intensity_map_count, 0)
         velo_processed[:, :, -1] = avg_intensity
         # velo_processed[:,:,-1] = np.where(intensity_map_count > 0.01, velo_processed[:, :, -1] / intensity_map_count, 0)
